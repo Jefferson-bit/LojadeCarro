c3a8de22322399d6202bdf57f31a4f042276b4ed
jefferson-bit <jhonsonbabi@hotmail.com>
2020-07-27 20:14:34 -0300

    Paginando categorias

--- src/main/java/com/crash/service/CategoriaService.java
+++ src/main/java/com/crash/service/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.crash.domain.Categoria;
@@ -32,5 +35,10 @@ public class CategoriaService {
 	
 	public Categoria fromDto(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getMarca());
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pgRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pgRequest);
 	}
 }
