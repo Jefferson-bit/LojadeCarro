538294ba07f58d97f2dd188f50a5a893d547e03d
jefferson-bit <jhonsonbabi@hotmail.com>
2020-07-27 18:43:22 -0300

    Inicial commit

---
+++ src/main/java/com/crash/LojaDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.crash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeCarrosApplication.class, args);
+	}
+
+}
