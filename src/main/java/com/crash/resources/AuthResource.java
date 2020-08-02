0e7bdd5af0690da5186154a5152c3f015e9c43da
jefferson-bit <jhonsonbabi@hotmail.com>
2020-08-02 20:38:22 -0300

    Refresh Token

--- src/main/java/com/crash/resources/AuthResource.java
+++ src/main/java/com/crash/resources/AuthResource.java
@@ -1,5 +1,6 @@
 package com.crash.resources;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crash.domain.dto.EmailDTO;
+import com.crash.security.JWTUtil;
+import com.crash.security.UserSS;
 import com.crash.service.AuthService;
+import com.crash.service.UserService;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -19,10 +23,21 @@ public class AuthResource {
 	@Autowired
 	private AuthService service;
 	
+	@Autowired 
+	private JWTUtil jwtUtil;
+	
 	@PostMapping(value = "/forgot")
 	public ResponseEntity<Void>forgot(@Valid @RequestBody EmailDTO objDto){
 		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.ok().build();
 	}
 	
+	@PostMapping(value = "/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
 }
